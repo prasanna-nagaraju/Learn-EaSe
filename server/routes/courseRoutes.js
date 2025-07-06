527090bccfe3f2405f45d94990042b7ad4ecbdda
Guru Preetam Bodapati <gurupreetambodapati@gmail.com>
2025-07-06 11:22:52 +0530

    Moved my-courses method above route id

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -13,13 +13,13 @@
     router.route('/')
         .get(getCourses)
         .post(protect, authorizeRoles('instructor', 'admin'), createCourse);
+    
+    router.get('/my-courses', protect, authorizeRoles('instructor'), getMyCourses);
 
     router.route('/:id')
         .get(getCourseById)
         .put(protect, authorizeRoles('instructor', 'admin'), updateCourse)
         .delete(protect, authorizeRoles('instructor', 'admin'), deleteCourse);
-
-    router.get('/my-courses', protect, authorizeRoles('instructor'), getMyCourses);
 
     module.exports = router;
     
